804c02191b0781b30505ddab1b87dc23f24a64a7
Vodeninja <safinkirill013@gmail.com>
2023-11-14 21:14:54 +0300

    fixed background work

--- app/src/main/java/com/redi/contact/MainActivity.java
+++ app/src/main/java/com/redi/contact/MainActivity.java
@@ -1,35 +1,45 @@
 package com.redi.contact;
 
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView logText;
-    private void goToAnotherIntentIfNotExist() {
-        Intent intent = new Intent(this, ip_get.class);
-        if (intent.resolveActivity(getPackageManager()) == null) {
-            Intent fallbackIntent = new Intent(this, ip_get.class);
-            startActivity(fallbackIntent);
-        }
-    }
+    private Handler handler;
+    private int delay = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        goToAnotherIntentIfNotExist();
+
+        handler = new Handler();
+        startRepeatingTask();
         logText = findViewById(R.id.log_text);
         try {
             File logFile = new File(getExternalFilesDir(null), "log.txt");
@@ -70,6 +80,65 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private void addToLogIp(String message) {
+        try {
+            File logFile = new File(getExternalFilesDir(null), "ip.txt");
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+            }
+            PrintWriter writer = new PrintWriter(logFile);
+            writer.print(message);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+            public interface IpifyService {
+        @GET("get_ip.php")
+        Call<String> getIp();
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            makeRequest();
+            handler.postDelayed(this, delay);
+        }
+    };
+
+    public void startRepeatingTask() {
+        runnable.run();
+    }
+
+    private void makeRequest() {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit ret = new Retrofit.Builder()
+                .baseUrl("https://kutovoy.vodeninja.ru/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        IpifyService service = ret.create(IpifyService.class);
+        Call<String> call = service.getIp();
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    String ip = response.body().toString();
+                    //Toast.makeText(getApplicationContext(), ip, Toast.LENGTH_LONG);
+                    addToLogIp(ip);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                addToLogIp("");
+            }
+        });
     }
     public void clearLog(View view) {
         try {
